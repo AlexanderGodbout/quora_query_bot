7ddddee1311241b3349e5e662895af632f3f313b
AlexanderGodbout <aggodbout@ucdavis.edu>
2020-10-17 19:42:08 -0700

    Finished Scraper V1

--- ques_db.py
+++ ques_db.py
@@ -11,71 +11,91 @@ class ques_db:
                     ,passwd="mmlja5ja5ja%"
                     ,db="quora_query_db" 
                     ,charset='utf8mb4'
+                    ,autocommit=True
                 )
         self.cursor = db.cursor() 
 
-    def create_table(self, desc, table): 
-        query = 'CREATE TABLE IF NOT EXISTS ' + table + '(\n\t'
-        for col_name, col_type in desc.items(): 
+
+    def create_table(self, Table): 
+        query = 'CREATE TABLE IF NOT EXISTS ' + Table.name + '(\n\t'
+        for col_name, col_type in Table.desc.items(): 
             query += col_name + ' ' + col_type + ',\n\t'
         query = query[:-3] + '\n );'
-        print(query)
-        #self.cursor.execute(query)
-
-    def insert(self, data, table):
-        query =  'INSERT INTO '+ table \
-            +'('+ str(list(data.keys())).strip("[]").replace("'","") + ')' \
-            + ' VALUES (' 
-        for record in data: 
-            query += str(list(data.values())).strip('[]') 
-        query += + ')'
-
-        query = query.replace('None','NULL')
         self.cursor.execute(query)
 
 
-if __name__ == '__main__':
+    def insert(self, Table, records):
+        query =  'INSERT INTO '+ Table.name \
+            +'('+ str(list(Table.desc.keys())).strip("[]").replace("'","") + ')\n' \
+            + ' VALUES \n' 
+        for record in records: 
+            query += '('
+            for field in Table.desc.keys(): 
+                cell = str(record.get(field, 'Null')).replace("'","")
+                cell = cell if cell in ('Null','Now()') else "'" + cell + "'"
+                query +=  cell + ', '
+            query = query[:-2]  + '),\n' 
+        query = query[:-2] + ';'
+    
+        self.cursor.execute(query)
+        print('Records inserted')
+      
 
-    scrapes_desc = {
-        'id':           'INT AUTO_INCREMENT PRIMARY KEY' 
-        ,'question':    'VARCHAR(255)'
-        ,'website' :    'CHAR(10)'
-        ,'topic':       'VARCHAR(255)'
-        ,'version':     'VARCHAR(255)'
-        ,'timestamp':   'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
-    }
+class Table: 
+    def __init__(self, table): 
+        self.name = table
 
-    posts_desc = {
-        'id':               'INT NOT NULL'
-        ,'account':         'VARCHAR(255)'
-        ,'topics':          'VARCHAR(255)'
-        ,'answers':         'INT'
-        ,'followers':       'INT'
-        ,'views':           'INT'
-        ,'ad_impressions':  'INT'
-        ,'ques_earn':       'DECIMAL(12, 2)'
-        ,'req_earn':        'DECIMAL(12, 2)'
-        ,'tot_earn':        'DECIMAL(12, 2)'
-        ,'grammatical':     'TINYINT(1)'
-        ,'unique':          'TINYINT(1)'
-        ,'version':         'VARCHAR(255)'
-        ,'timestamp':       'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
-    }
+        if table == 'scrapes': 
+            self.desc = {
+                'id':           'INT AUTO_INCREMENT PRIMARY KEY' 
+                ,'question':    'VARCHAR(255)'
+                ,'website' :    'CHAR(10)'
+                ,'topic':       'VARCHAR(255)'
+                ,'version':     'VARCHAR(255)'
+                ,'timestamp':   'TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL'
+            }
 
-    predicts_desc ={
-        'id':               'INT NOT NULL'
-        ,'pred_views':      'INT'
-        ,'pred_topics':     'VARCHAR(255)'
-        ,'pred_grammatical':'TINYINT(1)'
-        ,'pred_unique':     'TINYINT(1)'
-        ,'pred_views':      'INT'
-        ,'pred_tot_earn':   'DECIMAL(12, 2)'
-        ,'features_id':     'INT'
-        ,'version':         'VARCHAR(255)'
-        ,'timestamp':       'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
-    }
+        if table == 'posts': 
+            self.desc =  {
+                'id':               'INT NOT NULL'
+                ,'account':         'VARCHAR(255)'
+                ,'topics':          'VARCHAR(255)'
+                ,'answers':         'INT'
+                ,'followers':       'INT'
+                ,'views':           'INT'
+                ,'ad_impressions':  'INT'
+                ,'ques_earn':       'DECIMAL(12, 2)'
+                ,'req_earn':        'DECIMAL(12, 2)'
+                ,'tot_earn':        'DECIMAL(12, 2)'
+                ,'grammatical':     'TINYINT(1)'
+                ,'is_unique':       'TINYINT(1)'
+                ,'version':         'VARCHAR(255)'
+                ,'timestamp':       'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+            }
+
+        if table == 'predicts': 
+            self.desc = { 
+                'id':               'INT NOT NULL'
+                ,'pred_views':      'INT'
+                ,'pred_topics':     'VARCHAR(255)'
+                ,'pred_grammatical':'TINYINT(1)'
+                ,'pred_unique':     'TINYINT(1)'
+                ,'pred_views':      'INT'
+                ,'pred_tot_earn':   'DECIMAL(12, 2)'
+                ,'features_id':     'INT'
+                ,'version':         'VARCHAR(255)'
+                ,'timestamp':       'TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+            }
+
+    
+def create_database(): 
 
     db = ques_db()
-    db.create_table(scrapes_desc, 'scrapes') 
-    db.create_table(posts_desc, 'posts') 
-    db.create_table(predicts_desc, 'predicts') 
+    db.create_table(Table('scrapes')) 
+    db.create_table(Table('posts') )
+    db.create_table(Table('predicts'))
+
+
+
+
+
