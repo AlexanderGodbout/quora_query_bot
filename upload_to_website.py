cdf46f8d8c0ad1251e7e2e564f9d9e150802f019
LyonellBeckett <lyonellbeckett@gmail.com>
2020-10-22 07:48:08 -0700

    Tested Windows bash

--- upload_to_website.py
+++ upload_to_website.py
@@ -1,6 +1,21 @@
 import os
 import time 
+import subprocess
 
-while True: 
-    time.sleep(60)
-    os.system('bash update_website.sh') 
+import signal 
+
+
+devnull = open('./null.txt', 'w') 
+p = subprocess.Popen(["./main"], stdout=devnull, shell=False) 
+
+pid = p.pid 
+
+#while True: 
+#    time.sleep(60)
+os.system('bash update_website.sh')
+
+time.sleep(120)
+os.kill(pid, signal.SIGINT)
+
+if not p.poll():
+    print("Process correctly halted") 
